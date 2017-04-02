3400f240dbef8e4158ad2e240cb1b8b7e9315c27
daniloviic <danilovic.milos96@gmail.com>
2017-04-02 23:02:16 +0200

    Izvrsena implementacija metoda interfejsa MenjacnicaInterfejs u okviru grane implementacijainterfejsa.

--- Menjacnica/src/menjacnica/Menjacnica.java
+++ Menjacnica/src/menjacnica/Menjacnica.java
@@ -1,26 +1,43 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import menjacnicaInterfejs.MenjacnicaInterfejs;
 
 public class Menjacnica implements MenjacnicaInterfejs{
 
+	private LinkedList<Valuta> kursnaLista = new LinkedList<Valuta>();
 	@Override
 	public void dodajKurs(Valuta valuta, double prodajni, double kupovni, double srednji, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		valuta.setProdajni(prodajni);
+		valuta.setKupovni(kupovni);
+		valuta.setSrednji(srednji);
+		valuta.setDatum(datum);
 		
+		kursnaLista.add(valuta);
 	}
 
 	@Override
 	public void obrisiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		valuta.setDatum(datum);
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if(kursnaLista.get(i).equals(valuta)){
+				kursnaLista.remove(i);
+				return;
+			}
+		}
 		
 	}
 
 	@Override
 	public double vratiKurs(Valuta valuta, GregorianCalendar datum) {
-		// TODO Auto-generated method stub
+		valuta.setDatum(datum);
+		for (int i = 0; i < kursnaLista.size(); i++) {
+			if(kursnaLista.get(i).equals(valuta)){
+				return kursnaLista.get(i).getSrednji();
+			}
+		}
 		return 0;
 	}
 
